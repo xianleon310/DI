277dde380948321348910be9f96390a55a49c7ca
XianLeon <xleonr23@fpcoruna.afundacion.org>
2024-10-01 14:29:54 +0200

    1.Funciones suma,resta,multiplicacion y division

---
+++ sprint0python/operaciones.py
@@ -0,0 +1,13 @@
+def suma(a, b):
+    return a + b
+
+def resta(a, b):
+    return a - b
+
+def multiplicacion(a, b):
+    return a * b
+
+def division(a, b):
+    if b == 0:
+        return "Error: División por 0"
+    return a / b
