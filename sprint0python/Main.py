39946600169993bcaa3d6f882e1534c0dd91b486
XianLeon <xleonr23@fpcoruna.afundacion.org>
2024-10-10 13:31:35 +0200

    Clase Main

---
+++ sprint0python/Main.py
@@ -0,0 +1,12 @@
+from Heroe import Heroe
+from Mazmorra import Mazmorra
+
+
+def main():
+    nombre_heroe=input("Introduce el nombre de tu héroe: ")
+    heroe= Heroe(nombre_heroe)
+    mazmorra=Mazmorra(heroe)
+    mazmorra.jugar()
+
+if __name__ == "__main__":
+    main()
