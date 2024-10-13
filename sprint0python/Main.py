e14f088d4f64aec030dff4d248156240ae365448
xianleon310 <xleonr23@fpcoruna.afundacion.org>
2024-10-13 13:48:50 +0200

    Main.py

--- sprint0python/Main.py
+++ sprint0python/Main.py
@@ -3,9 +3,22 @@ from Mazmorra import Mazmorra
 
 
 def main():
+    #Guardamos en la variable "nombre_heroe" lo que escribamos vía teclado
     nombre_heroe=input("Introduce el nombre de tu héroe: ")
+
+
+    #Creamos un objeto "heroe", en el cual se guardarán los datos de la clase "Heroe" que llevará el nombre que escribamos vía teclado
+    #-CLASE HÉROE->*
     heroe= Heroe(nombre_heroe)
+
+
+    #Creamos una variable "mazmorra", en la cual se guardará los datos de la clase "Mazmorra" que llevará el objeto del héroa, llamado "heroe"
+    #-CLASE MAZMORRA->*
     mazmorra=Mazmorra(heroe)
+
+
+    #Viajamos al método "jugar" de la clase mazmorra
+    #-CLASE MAZMORRA ->*
     mazmorra.jugar()
 
 if __name__ == "__main__":
