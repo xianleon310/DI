918f9e04ade1ea195db9a1f2fea2cd9d43645b30
xianleon310 <xleonr23@fpcoruna.afundacion.org>
2024-11-25 22:40:12 +0100

    Iconos

---
+++ sprint3android/app/src/main/java/com/example/mycatalog/FavoritosFragment.java
@@ -0,0 +1,39 @@
+package com.example.mycatalog;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class FavoritosFragment extends Fragment {
+
+
+    public FavoritosFragment() {
+        // Required empty public constructor
+    }
+
+    public static FavoritosFragment newInstance(String param1, String param2) {
+        FavoritosFragment fragment = new FavoritosFragment();
+
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_favoritos, container, false);
+    }
+}
--- sprint3android/app/src/main/java/com/example/mycatalog/MainActivity.java
+++ sprint3android/app/src/main/java/com/example/mycatalog/MainActivity.java
@@ -7,18 +7,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+import androidx.navigation.ui.NavigationUiSaveStateControl;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        setupNavegacion();
+    }
+
+    private void setupNavegacion() {
+        BottomNavigationView bottomNavigationView=findViewById(R.id.bottomNavigationView);
+        NavHostFragment navHostFragment= (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_hostfragment);
+        NavigationUI.setupWithNavController(
+                bottomNavigationView,
+                navHostFragment.getNavController()
+        );
     }
 }
