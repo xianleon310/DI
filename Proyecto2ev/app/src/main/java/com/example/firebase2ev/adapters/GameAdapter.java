7f714d275911e5fdd7600e4d1bd365d2ba8d1ccc
xianleon310 <xleonr23@fpcoruna.afundacion.org>
2025-01-27 17:26:07 +0100

    Semana2:layout terminado

---
+++ Proyecto2ev/app/src/main/java/com/example/firebase2ev/adapters/GameAdapter.java
@@ -0,0 +1,75 @@
+package com.example.firebase2ev.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.firebase2ev.R;
+import com.example.firebase2ev.models.Game;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameAdapter extends RecyclerView.Adapter<GameAdapter.GameViewHolder> {
+    private List<Game> games = new ArrayList<>();
+    private OnGameClickListener listener;
+
+    public interface OnGameClickListener {
+        void onGameClick(Game game);
+    }
+
+    public GameAdapter(OnGameClickListener listener) {
+        this.listener = listener;
+    }
+
+    public void setGames(List<Game> games) {
+        this.games = games;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public GameViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_game, parent, false);
+        return new GameViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull GameViewHolder holder, int position) {
+        Game game = games.get(position);
+        holder.bind(game);
+    }
+
+    @Override
+    public int getItemCount() {
+        return games.size();
+    }
+
+    class GameViewHolder extends RecyclerView.ViewHolder {
+        private ImageView gameImage;
+        private TextView gameTitle;
+
+        public GameViewHolder(@NonNull View itemView) {
+            super(itemView);
+            gameImage = itemView.findViewById(R.id.gameImage);
+            gameTitle = itemView.findViewById(R.id.gameTitle);
+        }
+
+        public void bind(Game game) {
+            gameTitle.setText(game.getName());
+            Glide.with(itemView.getContext())
+                    .load(game.getUrl())
+                    .fitCenter()
+                    .into(gameImage);
+
+            itemView.setOnClickListener(v -> listener.onGameClick(game));
+        }
+    }
+}
